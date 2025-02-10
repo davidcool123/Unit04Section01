e0895756987d1d50c56aa892415fa83f2ba7a504
David Peterson <peterdav000@warren.k12.in.us>
2025-02-10 12:53:50 -0500

    lebron

--- src/main.java
+++ src/main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class main {
+import java.util.ArrayList;
+import java.util.Collections;
+
+class main{
+    public static void main(String[] args) {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Date");
+        list.add("Elderberry");
+
+        list.remove(2);
+
+        Collections.sort(list);
+
+        System.out.println("Modified List " + list);
+    }
 }
+
+
+
